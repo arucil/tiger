833c50547b7f3759bd830ce5578f3a8c528ce138
amlo <xplzjwz@gmail.com>
2019-02-04 10:53:20 +0800

    Add tests for lexer error report

--- test/lex/test_lex.ml
+++ test/lex/test_lex.ml
@@ -3,39 +3,61 @@ open Base
 open OUnit2
 open Lex
 open Token
+open Stdio
 
 let run_lexer s =
+  let temp_file = Caml.Filename.temp_file "tiger_lex_" "" in
+  let oc = Out_channel.create ~append:false temp_file in
+  Errors.set_out oc;
   let lexbuf = Lexing.from_string s in
   let rec go () =
     match Lexer.get_token lexbuf with
     | Token.EOF _ as t -> [t]
     | t -> t :: go ()
   in
-    go ()
+  let result = go () in
+  Out_channel.close oc;
+  let errors = In_channel.read_all temp_file in
+  (result, errors)
 
-let assert_ok (src : string) (expected : token list) =
-  let actual = run_lexer src in
-  if Poly.(actual = expected) then
+let assert_result (src : string) (expected_tokens : token list) (expected_errors : string) =
+  let expected_errors = String.lstrip expected_errors in
+  let (actual, errors) = run_lexer src in
+  if Poly.(actual = expected_tokens && errors = expected_errors) then
     ()
   else
     assert_failure
       (Printf.sprintf {|=========== expected ==========
 
+tokens:
+
+%s
+
+errors:
+
 %s
 
 ========== actual ==========
 
+tokens:
+
+%s
+
+errors:
+
 %s
 |}
-         ([%derive.show: token list] expected)
-         ([%derive.show: token list] actual))
+         ([%derive.show: token list] expected_tokens)
+         expected_errors
+         ([%derive.show: token list] actual)
+         errors)
 
 let test_lex =
   "Test Lexer" >::: [
 
 
     "keywords" >:: (fun _ ->
-      assert_ok {|
+      assert_result {|
   while  for      to
   break  let      in
   end    function var
@@ -51,11 +73,12 @@ let test_lex =
         THEN (6, 3); ELSE (6, 10); DO (6, 19);
         OF (7, 3); NIL (7, 10);
         EOF (8, 1);
-      ]);
+      ]
+      "");
 
 
     "operators" >:: (fun _ ->
-      assert_ok {|
+      assert_result {|
   +-*/=:=,:;()[]{}>>=<<><=|&
 |}
       [
@@ -66,11 +89,12 @@ let test_lex =
         GE (2, 20); LT (2, 22); NEQ (2, 23); LE (2, 25); OR (2, 27);
         AND (2, 28);
         EOF (3, 1)
-      ]);
+      ]
+      "");
 
 
     "identifier" >:: (fun _ ->
-      assert_ok {|
+      assert_result {|
 abc123  _Hello  __
 a_B_c_1_2_3_.L1+If
 |}
@@ -79,11 +103,12 @@ a_B_c_1_2_3_.L1+If
         ID ((3, 1), "a_B_c_1_2_3_"); DOT (3, 13);
         ID ((3, 14), "L1"); PLUS (3, 16); ID ((3, 17), "If");
         EOF (4, 1)
-      ]);
+      ]
+      "");
 
 
     "number" >:: (fun _ ->
-      assert_ok {| 123  5624214   -000234 000
+      assert_result {| 123  5624214   -000234 000
 12000 |}
         [
           INT ((1, 2), 123); INT ((1, 7), 5624214);
@@ -91,20 +116,22 @@ a_B_c_1_2_3_.L1+If
           INT ((1, 18), 234); INT ((1, 25), 0);
           INT ((2, 1), 12000);
           EOF (2, 7)
-        ]);
+        ]
+        "");
 
 
     "comment" >::: [
       "plain" >:: (fun _ ->
-        assert_ok {|
+        assert_result {|
   /* */
   /*comment*/   /***//***
 ****/
 |}
           [
             EOF (5, 1)
-          ];
-        assert_ok {|127
+          ]
+          "";
+        assert_result {|127
 /*  outer* / * /*
  inner 
  */*
@@ -113,12 +140,13 @@ a_B_c_1_2_3_.L1+If
           [
             INT ((1, 1), 127); INT ((5, 13), 2);
             EOF (6, 1)
-          ])];
+          ]
+          "")];
 
 
     "string" >::: [
       "plain" >:: (fun _ ->
-        assert_ok {|
+        assert_result {|
 "abc""hey_12 +-"  "Wow!"
 |}
           [
@@ -127,10 +155,11 @@ a_B_c_1_2_3_.L1+If
             STR ((2, 19), "Wow!");
             EOF (3, 1)
           ]
+          ""
       );
 
       "char escape" >:: (fun _ ->
-        assert_ok {|
+        assert_result {|
 "ok:\"\\abc\"" "ABC\tDEF\nxyz"
 |}
           [
@@ -138,19 +167,21 @@ a_B_c_1_2_3_.L1+If
             STR ((2, 16), "ABC\tDEF\nxyz");
             EOF (3, 1)
           ]
+          ""
       );
 
       "ascii escape" >:: (fun _ ->
-        assert_ok {|
+        assert_result {|
 "\097b\0999d"
 |}
           [
             STR ((2, 1), "abc9d"); EOF (3, 1)
           ]
+          ""
       );
 
       "line-spanning escape" >:: (fun _ ->
-        assert_ok {|
+        assert_result {|
 "abc\      \xyz"
   "abc\
   \def\
@@ -165,11 +196,12 @@ a_B_c_1_2_3_.L1+If
             STR ((5, 10), "123456");
             EOF (9, 1)
           ]
+          ""
         );
     ];
 
     "mixed" >:: (fun _ ->
-      assert_ok {|
+      assert_result {|
 let
   var foo : int := 30
   function fib(x : int) : int =
@@ -197,7 +229,39 @@ end
           (Token.ID ((7, 7), "foo")); (Token.RPAREN (7, 10)); (Token.END (8, 1));
           (Token.EOF (9, 1))
         ]
+        ""
     );
+
+    "errors" >::: [
+      "string 1" >:: (fun _ ->
+        assert_result
+          {|"abc
+"opq\999\xy" "xyz|}
+          [(Token.STR ((1, 1), "abc")); (Token.STR ((2, 1), "opqy"));
+          (Token.STR ((2, 14), "xyz")); (Token.EOF (2, 18))]
+          {|
+Error at (1:1): unclosed string
+Error at (2:6): ASCII code out ot range
+Error at (2:10): invalid escape
+Error at (2:14): unclosed string
+|});
+
+      "string 2" >:: (fun _ ->
+        assert_result
+          {|"abc\|}
+          [(Token.STR ((1, 1), "abc")); (Token.EOF (1, 6))]
+          {|
+Error at (1:1): unclosed string
+|});
+
+      "comment" >:: (fun _ ->
+        assert_result
+          {|/*  /*  /*abc*/|}
+          [Token.EOF (1, 16)]
+          {|
+Error at (1:5): unclosed comment
+|})
+    ];
   ]
 
 let () =
