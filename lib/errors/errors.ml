d3e1ddb2cb4f6ff7e9237019deef1138a3a709c6
amlo <xplzjwz@gmail.com>
2019-02-10 20:01:45 +0800

    Remove has_errors

--- lib/errors/errors.ml
+++ lib/errors/errors.ml
@@ -10,12 +10,9 @@ type pos = line_num * col_num [@@deriving show]
 
 let oc = ref Stdio.stderr
 
-let erroneous = ref false
-
 let source_name = ref "unknown"
 
 let report (line, col) fmt =
-  erroneous := true;
   Stdio.Out_channel.fprintf !oc "%s:%d:%d: error: " !source_name line col;
   Stdio.Out_channel.kfprintf
     (fun oc -> Stdio.Out_channel.newline oc)
@@ -25,8 +22,6 @@ let set_out oc' =
   oc := oc'
 
 let out () = !oc
-
-let has_errors () = !erroneous
 
 let set_source_name name =
   source_name := name
