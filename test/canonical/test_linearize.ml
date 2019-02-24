bfd8113615960335982cbd86aa80f6e1559e14fb
amlo <xplzjwz@gmail.com>
2019-02-24 18:21:19 +0800

    Tested linearize

--- test/canonical/test_linearize.ml
+++ test/canonical/test_linearize.ml
@@ -35,15 +35,19 @@ let stmt_eq s1 s2 = String.(Ir.show_stmt s1 = Ir.show_stmt s2)
 
 let show_stmts stmts =
   let rec go buf = function
-    | [] -> Buffer.contents buf
+    | [] -> ()
     | s :: ss ->
       begin
         Buffer.add_string buf (Ir.show_stmt s);
-        Buffer.add_char buf '\n';
+        Buffer.add_string buf ";\n";
         go buf ss;
       end
   in
-      go (Buffer.create 10) stmts
+  let buf = Buffer.create 10 in
+  Buffer.add_string buf "[\n";
+  go buf stmts;
+  Buffer.add_string buf "]\n";
+  Buffer.contents buf
 
 let assert_ok (src : string) (expected_stmts : Ir.stmt list) =
   let (_, stmts) = run_linearize src in
@@ -64,7 +68,104 @@ Statements:
       (show_stmts stmts))
 
 let test_linearize =
+  let print = Symbol.sym "print" in
+  let chr = Symbol.sym "chr" in
+  let ord = Symbol.sym "ord" in
+  let alloc_array = Symbol.sym "alloc_array" in
+  let getchar = Symbol.sym "getchar" in
+  let t100 = Temp.temp_of_int 100 in
+  let t101 = Temp.temp_of_int 101 in
+  let t102 = Temp.temp_of_int 102 in
+  let t103 = Temp.temp_of_int 103 in
+  let _L0 = Temp.named_label "_L0" in
+  let _L1 = Temp.named_label "_L1" in
+
   "Test Linearize" >::: [
+
+    "flatten Seq" >:: (fun _ ->
+      assert_ok
+        {|let var a := 2 in a := a + 1; print("h"); a end|}
+        [
+(Ir.Move ((Ir.Temp t100), (Ir.Const 2)));
+(Ir.Move ((Ir.Temp t100),
+   (Ir.Binop (Ir.Add, (Ir.Lval (Ir.Temp t100)), (Ir.Const 1)))));
+(Ir.Expr (Ir.Call ((Ir.Name print), [(Ir.Name _L0)])));
+(Ir.Expr (Ir.Lval (Ir.Temp t100)));
+]
+        );
+
+    "lift stmt in binary" >:: (fun _ ->
+      assert_ok
+        {|let var a := 2 in (a := 3; a - 1) + (print("abc"); 307; 2 * 4) end|}
+        [
+(Ir.Move ((Ir.Temp t100), (Ir.Const 2)));
+(Ir.Move ((Ir.Temp t100), (Ir.Const 3)));
+(Ir.Move ((Ir.Temp t101),
+   (Ir.Binop (Ir.Sub, (Ir.Lval (Ir.Temp t100)), (Ir.Const 1)))));
+(Ir.Expr (Ir.Call ((Ir.Name print), [(Ir.Name _L0)])));
+(Ir.Expr
+   (Ir.Binop (Ir.Add, (Ir.Lval (Ir.Temp t101)),
+      (Ir.Binop (Ir.Mul, (Ir.Const 2), (Ir.Const 4))))));
+]
+        );
+
+    "lift stmt in binary w/o temp" >:: (fun _ ->
+      assert_ok
+        {|let var a := 2 in (a := 3; 7) + (print("abc"); 307; 2 * 4) end|}
+        [
+(Ir.Move ((Ir.Temp t100), (Ir.Const 2)));
+(Ir.Move ((Ir.Temp t100), (Ir.Const 3)));
+(Ir.Expr (Ir.Call ((Ir.Name print), [(Ir.Name _L0)])));
+(Ir.Expr
+   (Ir.Binop (Ir.Add, (Ir.Const 7),
+      (Ir.Binop (Ir.Mul, (Ir.Const 2), (Ir.Const 4))))));
+]
+        );
+
+    "flatten function call" >:: (fun _ ->
+      assert_ok
+        {|let var a := () in a := print(chr(3 + ord("a"))) end|}
+        [
+(Ir.Move ((Ir.Temp t100), (Ir.Const 0)));
+(Ir.Move ((Ir.Temp t102), (Ir.Call ((Ir.Name ord), [(Ir.Name _L0)]))));
+(Ir.Move ((Ir.Temp t101),
+   (Ir.Call ((Ir.Name chr),
+      [(Ir.Binop (Ir.Add, (Ir.Const 3), (Ir.Lval (Ir.Temp t102))))]))
+   ));
+(Ir.Move ((Ir.Temp t100),
+   (Ir.Call ((Ir.Name print), [(Ir.Lval (Ir.Temp t101))]))));
+]
+        );
+
+    "lift stmt in index" >:: (fun _ ->
+      assert_ok
+        {|
+let
+  type a = array of int
+  var a := a[1] of ord("a")
+in
+  a[(getchar(); ord("012")*3)] := 37
+end
+        |}
+        [
+(Ir.Move ((Ir.Temp t101), (Ir.Call ((Ir.Name ord), [(Ir.Name _L0)]))));
+(Ir.Move ((Ir.Temp t100),
+   (Ir.Call ((Ir.Name alloc_array), [(Ir.Const 1); (Ir.Lval (Ir.Temp t101))]
+      ))
+   ));
+(Ir.Move ((Ir.Temp t103), (Ir.Lval (Ir.Temp t100))));
+(Ir.Expr (Ir.Call ((Ir.Name getchar), [])));
+(Ir.Move ((Ir.Temp t102), (Ir.Call ((Ir.Name ord), [(Ir.Name _L1)]))));
+(Ir.Move (
+   (Ir.Mem
+      (Ir.Binop (Ir.Add, (Ir.Lval (Ir.Temp t103)),
+         (Ir.Binop (Ir.Mul,
+            (Ir.Binop (Ir.Mul, (Ir.Lval (Ir.Temp t102)), (Ir.Const 3))),
+            (Ir.Const 4)))
+         ))),
+   (Ir.Const 37)));
+]
+        )
   ]
 
 let () =
