8aae4b5ce83a4b85c206e2a55aa9b221c6dec3a8
amlo <xplzjwz@gmail.com>
2020-09-24 17:33:13 +0800

    WIP: parser

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+
+parsergens::parsergen!(mod parser: LALR("parser.pg"));
+
+pub fn parse(input: &str) {
+    parser::with_input(input).parse_program();
 }
